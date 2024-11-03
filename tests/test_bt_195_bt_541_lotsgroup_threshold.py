7874d689d7c438465ee40100ecb609da263e234d
Bjørn Jørgensen <bjornjorgensen@gmail.com>
2024-11-03 12:01:34 +0000

    more tests fix

--- tests/test_bt_195_bt_541_lotsgroup_threshold.py
+++ tests/test_bt_195_bt_541_lotsgroup_threshold.py
@@ -4,6 +4,7 @@ import pytest
 import json
 import sys
 import logging
+import tempfile
 
 # Add the parent directory to sys.path to import main
 sys.path.append(str(Path(__file__).parent.parent))
@@ -16,52 +17,65 @@ def setup_logging():
     return logging.getLogger(__name__)
 
 
+@pytest.fixture
+def temp_output_dir():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        yield Path(tmpdirname)
+
+
+def run_main_and_get_result(xml_file, output_dir):
+    main(str(xml_file), str(output_dir), "ocds-test-prefix", "test-scheme")
+    output_files = list(output_dir.glob("*.json"))
+    assert len(output_files) == 1, f"Expected 1 output file, got {len(output_files)}"
+    with output_files[0].open() as f:
+        return json.load(f)
+
+
 def test_bt195_bt541_lotsgroup_threshold_unpublished_identifier_integration(
     tmp_path,
     setup_logging,
+    temp_output_dir,
 ):
     logger = setup_logging
-    xml_content = """
-    <root xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"
-          xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"
-          xmlns:ext="urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2"
-          xmlns:efac="http://data.europa.eu/p27/eforms-ubl-extension-aggregate-components/1"
-          xmlns:efext="http://data.europa.eu/p27/eforms-ubl-extensions/1"
-          xmlns:efbc="http://data.europa.eu/p27/eforms-ubl-extension-basic-components/1">
-        <cac:ProcurementProjectLot>
-            <cbc:ID schemeName="LotsGroup">GLO-0001</cbc:ID>
-            <cac:TenderingTerms>
-                <cac:AwardingTerms>
-                    <cac:AwardingCriterion>
-                        <cac:SubordinateAwardingCriterion>
-                            <ext:UBLExtensions>
-                                <ext:UBLExtension>
-                                    <ext:ExtensionContent>
-                                        <efext:EformsExtension>
-                                            <efac:AwardCriterionParameter>
-                                                <efbc:ParameterCode listName="number-threshold"/>
-                                                <efac:FieldsPrivacy>
-                                                    <efbc:FieldIdentifierCode>awa-cri-num</efbc:FieldIdentifierCode>
-                                                </efac:FieldsPrivacy>
-                                            </efac:AwardCriterionParameter>
-                                        </efext:EformsExtension>
-                                    </ext:ExtensionContent>
-                                </ext:UBLExtension>
-                            </ext:UBLExtensions>
-                        </cac:SubordinateAwardingCriterion>
-                    </cac:AwardingCriterion>
-                </cac:AwardingTerms>
-            </cac:TenderingTerms>
-        </cac:ProcurementProjectLot>
-    </root>
-    """
+    xml_content = """<?xml version="1.0" encoding="UTF-8"?>
+    <ContractNotice xmlns="urn:oasis:names:specification:ubl:schema:xsd:ContractNotice-2"
+    xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"
+    xmlns:ext="urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2"
+    xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"
+    xmlns:efac="http://data.europa.eu/p27/eforms-ubl-extension-aggregate-components/1"
+    xmlns:efext="http://data.europa.eu/p27/eforms-ubl-extensions/1"
+    xmlns:efbc="http://data.europa.eu/p27/eforms-ubl-extension-basic-components/1">
+    <cac:ProcurementProjectLot>
+        <cbc:ID schemeName="LotsGroup">GLO-0001</cbc:ID>
+        <cac:TenderingTerms>
+            <cac:AwardingTerms>
+                <cac:AwardingCriterion>
+                    <cac:SubordinateAwardingCriterion>
+                        <ext:UBLExtensions>
+                            <ext:UBLExtension>
+                                <ext:ExtensionContent>
+                                    <efext:EformsExtension>
+                                        <efac:AwardCriterionParameter>
+                                            <efbc:ParameterCode listName="number-threshold">awa-cri-num</efbc:ParameterCode>
+                                            <efac:FieldsPrivacy>
+                                                <efbc:FieldIdentifierCode>awa-cri-num</efbc:FieldIdentifierCode>
+                                            </efac:FieldsPrivacy>
+                                        </efac:AwardCriterionParameter>
+                                    </efext:EformsExtension>
+                                </ext:ExtensionContent>
+                            </ext:UBLExtension>
+                        </ext:UBLExtensions>
+                    </cac:SubordinateAwardingCriterion>
+                </cac:AwardingCriterion>
+            </cac:AwardingTerms>
+        </cac:TenderingTerms>
+    </cac:ProcurementProjectLot>
+    </ContractNotice>"""
+
     xml_file = tmp_path / "test_input_bt195_bt541_lotsgroup_threshold.xml"
     xml_file.write_text(xml_content)
 
-    main(str(xml_file), "ocds-test-prefix")
-
-    with Path("output.json").open() as f:
-        result = json.load(f)
+    result = run_main_and_get_result(xml_file, temp_output_dir)
 
     logger.info("Result: %s", json.dumps(result, indent=2))
 
@@ -82,76 +96,76 @@ def test_bt195_bt541_lotsgroup_threshold_unpublished_identifier_integration(
     ), "Unexpected name for withheld information"
 
 
-def test_bt195_bt541_lotsgroup_threshold_multiple_groups(tmp_path, setup_logging):
+def test_bt195_bt541_lotsgroup_threshold_multiple_groups(
+    tmp_path, setup_logging, temp_output_dir
+):
     logger = setup_logging
-    xml_content = """
-    <root xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"
-          xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"
-          xmlns:ext="urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2"
-          xmlns:efac="http://data.europa.eu/p27/eforms-ubl-extension-aggregate-components/1"
-          xmlns:efext="http://data.europa.eu/p27/eforms-ubl-extensions/1"
-          xmlns:efbc="http://data.europa.eu/p27/eforms-ubl-extension-basic-components/1">
-        <cac:ProcurementProjectLot>
-            <cbc:ID schemeName="LotsGroup">GLO-0001</cbc:ID>
-            <cac:TenderingTerms>
-                <cac:AwardingTerms>
-                    <cac:AwardingCriterion>
-                        <cac:SubordinateAwardingCriterion>
-                            <ext:UBLExtensions>
-                                <ext:UBLExtension>
-                                    <ext:ExtensionContent>
-                                        <efext:EformsExtension>
-                                            <efac:AwardCriterionParameter>
-                                                <efbc:ParameterCode listName="number-threshold"/>
-                                                <efac:FieldsPrivacy>
-                                                    <efbc:FieldIdentifierCode>awa-cri-num</efbc:FieldIdentifierCode>
-                                                </efac:FieldsPrivacy>
-                                            </efac:AwardCriterionParameter>
-                                        </efext:EformsExtension>
-                                    </ext:ExtensionContent>
-                                </ext:UBLExtension>
-                            </ext:UBLExtensions>
-                        </cac:SubordinateAwardingCriterion>
-                    </cac:AwardingCriterion>
-                </cac:AwardingTerms>
-            </cac:TenderingTerms>
-        </cac:ProcurementProjectLot>
-        <cac:ProcurementProjectLot>
-            <cbc:ID schemeName="LotsGroup">GLO-0002</cbc:ID>
-            <cac:TenderingTerms>
-                <cac:AwardingTerms>
-                    <cac:AwardingCriterion>
-                        <cac:SubordinateAwardingCriterion>
-                            <ext:UBLExtensions>
-                                <ext:UBLExtension>
-                                    <ext:ExtensionContent>
-                                        <efext:EformsExtension>
-                                            <efac:AwardCriterionParameter>
-                                                <efbc:ParameterCode listName="number-threshold"/>
-                                                <efac:FieldsPrivacy>
-                                                    <efbc:FieldIdentifierCode>awa-cri-num</efbc:FieldIdentifierCode>
-                                                </efac:FieldsPrivacy>
-                                            </efac:AwardCriterionParameter>
-                                        </efext:EformsExtension>
-                                    </ext:ExtensionContent>
-                                </ext:UBLExtension>
-                            </ext:UBLExtensions>
-                        </cac:SubordinateAwardingCriterion>
-                    </cac:AwardingCriterion>
-                </cac:AwardingTerms>
-            </cac:TenderingTerms>
-        </cac:ProcurementProjectLot>
-    </root>
-    """
+    xml_content = """<?xml version="1.0" encoding="UTF-8"?>
+    <ContractNotice xmlns="urn:oasis:names:specification:ubl:schema:xsd:ContractNotice-2"
+    xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"
+    xmlns:ext="urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2"
+    xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"
+    xmlns:efac="http://data.europa.eu/p27/eforms-ubl-extension-aggregate-components/1"
+    xmlns:efext="http://data.europa.eu/p27/eforms-ubl-extensions/1"
+    xmlns:efbc="http://data.europa.eu/p27/eforms-ubl-extension-basic-components/1">
+    <cac:ProcurementProjectLot>
+        <cbc:ID schemeName="LotsGroup">GLO-0001</cbc:ID>
+        <cac:TenderingTerms>
+            <cac:AwardingTerms>
+                <cac:AwardingCriterion>
+                    <cac:SubordinateAwardingCriterion>
+                        <ext:UBLExtensions>
+                            <ext:UBLExtension>
+                                <ext:ExtensionContent>
+                                    <efext:EformsExtension>
+                                        <efac:AwardCriterionParameter>
+                                            <efbc:ParameterCode listName="number-threshold">awa-cri-num</efbc:ParameterCode>
+                                            <efac:FieldsPrivacy>
+                                                <efbc:FieldIdentifierCode>awa-cri-num</efbc:FieldIdentifierCode>
+                                            </efac:FieldsPrivacy>
+                                        </efac:AwardCriterionParameter>
+                                    </efext:EformsExtension>
+                                </ext:ExtensionContent>
+                            </ext:UBLExtension>
+                        </ext:UBLExtensions>
+                    </cac:SubordinateAwardingCriterion>
+                </cac:AwardingCriterion>
+            </cac:AwardingTerms>
+        </cac:TenderingTerms>
+    </cac:ProcurementProjectLot>
+    <cac:ProcurementProjectLot>
+        <cbc:ID schemeName="LotsGroup">GLO-0002</cbc:ID>
+        <cac:TenderingTerms>
+            <cac:AwardingTerms>
+                <cac:AwardingCriterion>
+                    <cac:SubordinateAwardingCriterion>
+                        <ext:UBLExtensions>
+                            <ext:UBLExtension>
+                                <ext:ExtensionContent>
+                                    <efext:EformsExtension>
+                                        <efac:AwardCriterionParameter>
+                                            <efbc:ParameterCode listName="number-threshold">awa-cri-num</efbc:ParameterCode>
+                                            <efac:FieldsPrivacy>
+                                                <efbc:FieldIdentifierCode>awa-cri-num</efbc:FieldIdentifierCode>
+                                            </efac:FieldsPrivacy>
+                                        </efac:AwardCriterionParameter>
+                                    </efext:EformsExtension>
+                                </ext:ExtensionContent>
+                            </ext:UBLExtension>
+                        </ext:UBLExtensions>
+                    </cac:SubordinateAwardingCriterion>
+                </cac:AwardingCriterion>
+            </cac:AwardingTerms>
+        </cac:TenderingTerms>
+    </cac:ProcurementProjectLot>
+    </ContractNotice>"""
+
     xml_file = (
         tmp_path / "test_input_bt195_bt541_lotsgroup_threshold_multiple_groups.xml"
     )
     xml_file.write_text(xml_content)
 
-    main(str(xml_file), "ocds-test-prefix")
-
-    with Path("output.json").open() as f:
-        result = json.load(f)
+    result = run_main_and_get_result(xml_file, temp_output_dir)
 
     logger.info("Result: %s", json.dumps(result, indent=2))
 
@@ -180,34 +194,33 @@ def test_bt195_bt541_lotsgroup_threshold_multiple_groups(tmp_path, setup_logging
 def test_bt195_bt541_lotsgroup_threshold_no_unpublished_identifier(
     tmp_path,
     setup_logging,
+    temp_output_dir,
 ):
     logger = setup_logging
-    xml_content = """
-    <root xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"
-          xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2">
-        <cac:ProcurementProjectLot>
-            <cbc:ID schemeName="LotsGroup">GLO-0001</cbc:ID>
-            <cac:TenderingTerms>
-                <cac:AwardingTerms>
-                    <cac:AwardingCriterion>
-                        <cac:SubordinateAwardingCriterion>
-                            <!-- No unpublished identifier -->
-                        </cac:SubordinateAwardingCriterion>
-                    </cac:AwardingCriterion>
-                </cac:AwardingTerms>
-            </cac:TenderingTerms>
-        </cac:ProcurementProjectLot>
-    </root>
-    """
+    xml_content = """<?xml version="1.0" encoding="UTF-8"?>
+    <ContractNotice xmlns="urn:oasis:names:specification:ubl:schema:xsd:ContractNotice-2"
+    xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"
+    xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2">
+    <cac:ProcurementProjectLot>
+        <cbc:ID schemeName="LotsGroup">GLO-0001</cbc:ID>
+        <cac:TenderingTerms>
+            <cac:AwardingTerms>
+                <cac:AwardingCriterion>
+                    <cac:SubordinateAwardingCriterion>
+                        <!-- No unpublished identifier -->
+                    </cac:SubordinateAwardingCriterion>
+                </cac:AwardingCriterion>
+            </cac:AwardingTerms>
+        </cac:TenderingTerms>
+    </cac:ProcurementProjectLot>
+    </ContractNotice>"""
+
     xml_file = (
         tmp_path / "test_input_bt195_bt541_lotsgroup_threshold_no_unpublished.xml"
     )
     xml_file.write_text(xml_content)
 
-    main(str(xml_file), "ocds-test-prefix")
-
-    with Path("output.json").open() as f:
-        result = json.load(f)
+    result = run_main_and_get_result(xml_file, temp_output_dir)
 
     logger.info("Result: %s", json.dumps(result, indent=2))
 
--- tests/test_bt_195_bt_554_tender.py
+++ tests/test_bt_195_bt_554_tender.py
@@ -4,6 +4,7 @@ import pytest
 import json
 import sys
 import logging
+import tempfile
 
 # Add the parent directory to sys.path to import main
 sys.path.append(str(Path(__file__).parent.parent))
@@ -16,35 +17,47 @@ def setup_logging():
     return logging.getLogger(__name__)
 
 
-def test_bt_195_bt554_tender_integration(tmp_path, setup_logging):
+@pytest.fixture
+def temp_output_dir():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        yield Path(tmpdirname)
+
+
+def run_main_and_get_result(xml_file, output_dir):
+    main(str(xml_file), str(output_dir), "ocds-test-prefix", "test-scheme")
+    output_files = list(output_dir.glob("*.json"))
+    assert len(output_files) == 1, f"Expected 1 output file, got {len(output_files)}"
+    with output_files[0].open() as f:
+        return json.load(f)
+
+
+def test_bt_195_bt554_tender_integration(tmp_path, setup_logging, temp_output_dir):
     logger = setup_logging
-    xml_content = """
-    <root xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"
-          xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"
-          xmlns:ext="urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2"
-          xmlns:efac="http://data.europa.eu/p27/eforms-ubl-extension-aggregate-components/1"
-          xmlns:efext="http://data.europa.eu/p27/eforms-ubl-extensions/1"
-          xmlns:efbc="http://data.europa.eu/p27/eforms-ubl-extension-basic-components/1">
-        <efac:noticeResult>
-            <efac:LotTender>
-                <cbc:ID schemeName="result">TEN-0001</cbc:ID>
-                <efac:SubcontractingTerm>
-                    <efbc:TermCode listName="applicability">applicable</efbc:TermCode>
-                    <efac:FieldsPrivacy>
-                        <efbc:FieldIdentifierCode>sub-des</efbc:FieldIdentifierCode>
-                    </efac:FieldsPrivacy>
-                </efac:SubcontractingTerm>
-            </efac:LotTender>
-        </efac:noticeResult>
-    </root>
-    """
+    xml_content = """<?xml version="1.0" encoding="UTF-8"?>
+    <ContractNotice xmlns="urn:oasis:names:specification:ubl:schema:xsd:ContractNotice-2"
+    xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"
+    xmlns:ext="urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2"
+    xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"
+    xmlns:efac="http://data.europa.eu/p27/eforms-ubl-extension-aggregate-components/1"
+    xmlns:efext="http://data.europa.eu/p27/eforms-ubl-extensions/1"
+    xmlns:efbc="http://data.europa.eu/p27/eforms-ubl-extension-basic-components/1">
+    <efac:noticeResult>
+        <efac:LotTender>
+            <cbc:ID schemeName="result">TEN-0001</cbc:ID>
+            <efac:SubcontractingTerm>
+                <efbc:TermCode listName="applicability">applicable</efbc:TermCode>
+                <efac:FieldsPrivacy>
+                    <efbc:FieldIdentifierCode>sub-des</efbc:FieldIdentifierCode>
+                </efac:FieldsPrivacy>
+            </efac:SubcontractingTerm>
+        </efac:LotTender>
+    </efac:noticeResult>
+    </ContractNotice>"""
+
     xml_file = tmp_path / "test_input_bt195_bt554.xml"
     xml_file.write_text(xml_content)
 
-    main(str(xml_file), "ocds-test-prefix")
-
-    with Path("output.json").open() as f:
-        result = json.load(f)
+    result = run_main_and_get_result(xml_file, temp_output_dir)
 
     logger.info("Result: %s", json.dumps(result, indent=2))
 
@@ -64,33 +77,31 @@ def test_bt_195_bt554_tender_integration(tmp_path, setup_logging):
     ), f"Expected name 'Subcontracting Description', got {item['name']}"
 
 
-def test_bt_195_bt554_tender_missing_data(tmp_path, setup_logging):
+def test_bt_195_bt554_tender_missing_data(tmp_path, setup_logging, temp_output_dir):
     logger = setup_logging
-    xml_content = """
-    <root xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"
-          xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"
-          xmlns:ext="urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2"
-          xmlns:efac="http://data.europa.eu/p27/eforms-ubl-extension-aggregate-components/1"
-          xmlns:efext="http://data.europa.eu/p27/eforms-ubl-extensions/1"
-          xmlns:efbc="http://data.europa.eu/p27/eforms-ubl-extension-basic-components/1">
-        <efac:noticeResult>
-            <efac:LotTender>
-                <cbc:ID schemeName="result">TEN-0001</cbc:ID>
-                <efac:SubcontractingTerm>
-                    <efbc:TermCode listName="applicability">applicable</efbc:TermCode>
-                    <!-- Missing FieldsPrivacy element -->
-                </efac:SubcontractingTerm>
-            </efac:LotTender>
-        </efac:noticeResult>
-    </root>
-    """
+    xml_content = """<?xml version="1.0" encoding="UTF-8"?>
+    <ContractNotice xmlns="urn:oasis:names:specification:ubl:schema:xsd:ContractNotice-2"
+    xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"
+    xmlns:ext="urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2"
+    xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"
+    xmlns:efac="http://data.europa.eu/p27/eforms-ubl-extension-aggregate-components/1"
+    xmlns:efext="http://data.europa.eu/p27/eforms-ubl-extensions/1"
+    xmlns:efbc="http://data.europa.eu/p27/eforms-ubl-extension-basic-components/1">
+    <efac:noticeResult>
+        <efac:LotTender>
+            <cbc:ID schemeName="result">TEN-0001</cbc:ID>
+            <efac:SubcontractingTerm>
+                <efbc:TermCode listName="applicability">applicable</efbc:TermCode>
+                <!-- Missing FieldsPrivacy element -->
+            </efac:SubcontractingTerm>
+        </efac:LotTender>
+    </efac:noticeResult>
+    </ContractNotice>"""
+
     xml_file = tmp_path / "test_input_bt195_bt554_missing.xml"
     xml_file.write_text(xml_content)
 
-    main(str(xml_file), "ocds-test-prefix")
-
-    with Path("output.json").open() as f:
-        result = json.load(f)
+    result = run_main_and_get_result(xml_file, temp_output_dir)
 
     logger.info("Result: %s", json.dumps(result, indent=2))
 
