811af447210ce6f1395c4aafee78d2d91a6052fd
Bjørn Jørgensen <bjornjorgensen@gmail.com>
2025-03-01 15:36:51 +0100

    fix bt_144

--- tests/test_bt_144_lotresult.py
+++ tests/test_bt_144_lotresult.py
@@ -48,7 +48,7 @@ def test_bt_144_lotresult_integration(tmp_path, setup_logging, temp_output_dir) 
             <ext:UBLExtension>
                 <ext:ExtensionContent>
                     <efext:EformsExtension>
-                        <efac:noticeResult>
+                        <efac:NoticeResult>
                             <efac:LotResult>
                                 <cbc:ID schemeName="result">RES-0001</cbc:ID>
                                 <efac:DecisionReason>
@@ -58,7 +58,7 @@ def test_bt_144_lotresult_integration(tmp_path, setup_logging, temp_output_dir) 
                                     <cbc:ID schemeName="Lot">LOT-0001</cbc:ID>
                                 </efac:TenderLot>
                             </efac:LotResult>
-                        </efac:noticeResult>
+                        </efac:NoticeResult>
                     </efext:EformsExtension>
                 </ext:ExtensionContent>
             </ext:UBLExtension>
