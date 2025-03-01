f5621ac7d37033b2cebe66fc149b9a56dca467b1
Bjørn Jørgensen <bjornjorgensen@gmail.com>
2025-03-01 19:53:06 +0100

    fix bt-195-555

--- tests/test_bt_195_bt_555_tender.py
+++ tests/test_bt_195_bt_555_tender.py
@@ -46,17 +46,25 @@ def test_bt_195_bt555_tender_integration(
     xmlns:efac="http://data.europa.eu/p27/eforms-ubl-extension-aggregate-components/1"
     xmlns:efext="http://data.europa.eu/p27/eforms-ubl-extensions/1"
     xmlns:efbc="http://data.europa.eu/p27/eforms-ubl-extension-basic-components/1">
-    <efac:NoticeResult>
-        <efac:LotTender>
-            <cbc:ID schemeName="result">TEN-0001</cbc:ID>
-            <efac:SubcontractingTerm>
-                <efbc:TermCode listName="applicability">applicable</efbc:TermCode>
-                <efac:FieldsPrivacy>
-                    <efbc:FieldIdentifierCode listName="non-publication-identifier">sub-per</efbc:FieldIdentifierCode>
-                </efac:FieldsPrivacy>
-            </efac:SubcontractingTerm>
-        </efac:LotTender>
-    </efac:NoticeResult>
+    <ext:UBLExtensions>
+        <ext:UBLExtension>
+            <ext:ExtensionContent>
+                <efext:EformsExtension>
+                    <efac:NoticeResult>
+                        <efac:LotTender>
+                            <cbc:ID schemeName="result">TEN-0001</cbc:ID>
+                            <efac:SubcontractingTerm>
+                                <efbc:TermCode listName="applicability">applicable</efbc:TermCode>
+                                <efac:FieldsPrivacy>
+                                    <efbc:FieldIdentifierCode listName="non-publication-identifier">sub-per</efbc:FieldIdentifierCode>
+                                </efac:FieldsPrivacy>
+                            </efac:SubcontractingTerm>
+                        </efac:LotTender>
+                    </efac:NoticeResult>
+                </efext:EformsExtension>
+            </ext:ExtensionContent>
+        </ext:UBLExtension>
+    </ext:UBLExtensions>
     </ContractNotice>"""
 
     xml_file = tmp_path / "test_input_bt195_bt555.xml"
@@ -94,15 +102,22 @@ def test_bt_195_bt555_tender_missing_data(
     xmlns:efac="http://data.europa.eu/p27/eforms-ubl-extension-aggregate-components/1"
     xmlns:efext="http://data.europa.eu/p27/eforms-ubl-extensions/1"
     xmlns:efbc="http://data.europa.eu/p27/eforms-ubl-extension-basic-components/1">
-    <efac:NoticeResult>
-        <efac:LotTender>
-            <cbc:ID schemeName="result">TEN-0001</cbc:ID>
-            <efac:SubcontractingTerm>
-                <efbc:TermCode listName="applicability">applicable</efbc:TermCode>
-                <!-- Missing FieldsPrivacy element -->
-            </efac:SubcontractingTerm>
-        </efac:LotTender>
-    </efac:NoticeResult>
+    <ext:UBLExtensions>
+        <ext:UBLExtension>
+            <ext:ExtensionContent>
+                <efext:EformsExtension>
+                    <efac:NoticeResult>
+                        <efac:LotTender>
+                            <cbc:ID schemeName="result">TEN-0001</cbc:ID>
+                            <efac:SubcontractingTerm>
+                                <efbc:TermCode listName="applicability">applicable</efbc:TermCode>
+                            </efac:SubcontractingTerm>
+                        </efac:LotTender>
+                    </efac:NoticeResult>
+                </efext:EformsExtension>
+            </ext:ExtensionContent>
+        </ext:UBLExtension>
+    </ext:UBLExtensions>
     </ContractNotice>"""
 
     xml_file = tmp_path / "test_input_bt195_bt555_missing.xml"
@@ -115,6 +130,68 @@ def test_bt_195_bt555_tender_missing_data(
     assert (
         "withheldInformation" not in result or not result["withheldInformation"]
     ), "Did not expect 'withheldInformation' in result when data is missing"
+
+
+def test_bt_195_bt555_tender_multiple_items(
+    tmp_path, setup_logging, temp_output_dir
+) -> None:
+    logger = setup_logging
+    xml_content = """<?xml version="1.0" encoding="UTF-8"?>
+    <ContractNotice xmlns="urn:oasis:names:specification:ubl:schema:xsd:ContractNotice-2"
+    xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"
+    xmlns:ext="urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2"
+    xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"
+    xmlns:efac="http://data.europa.eu/p27/eforms-ubl-extension-aggregate-components/1"
+    xmlns:efext="http://data.europa.eu/p27/eforms-ubl-extensions/1"
+    xmlns:efbc="http://data.europa.eu/p27/eforms-ubl-extension-basic-components/1">
+    <ext:UBLExtensions>
+        <ext:UBLExtension>
+            <ext:ExtensionContent>
+                <efext:EformsExtension>
+                    <efac:NoticeResult>
+                        <efac:LotTender>
+                            <cbc:ID schemeName="result">TEN-0001</cbc:ID>
+                            <efac:SubcontractingTerm>
+                                <efbc:TermCode listName="applicability">applicable</efbc:TermCode>
+                                <efac:FieldsPrivacy>
+                                    <efbc:FieldIdentifierCode listName="non-publication-identifier">sub-per</efbc:FieldIdentifierCode>
+                                </efac:FieldsPrivacy>
+                            </efac:SubcontractingTerm>
+                        </efac:LotTender>
+                        <efac:LotTender>
+                            <cbc:ID schemeName="result">TEN-0002</cbc:ID>
+                            <efac:SubcontractingTerm>
+                                <efbc:TermCode listName="applicability">applicable</efbc:TermCode>
+                                <efac:FieldsPrivacy>
+                                    <efbc:FieldIdentifierCode listName="non-publication-identifier">sub-per</efbc:FieldIdentifierCode>
+                                </efac:FieldsPrivacy>
+                            </efac:SubcontractingTerm>
+                        </efac:LotTender>
+                    </efac:NoticeResult>
+                </efext:EformsExtension>
+            </ext:ExtensionContent>
+        </ext:UBLExtension>
+    </ext:UBLExtensions>
+    </ContractNotice>"""
+
+    xml_file = tmp_path / "test_input_bt195_bt555_multiple.xml"
+    xml_file.write_text(xml_content)
+
+    result = run_main_and_get_result(xml_file, temp_output_dir)
+
+    assert "withheldInformation" in result, "Expected 'withheldInformation' in result"
+    withheld_info = result["withheldInformation"]
+    assert len(withheld_info) == 2, f"Expected 2 withheld information items, got {len(withheld_info)}"
+
+    # Check first item
+    assert withheld_info[0]["id"] == "sub-per-TEN-0001"
+    assert withheld_info[0]["field"] == "sub-per"
+    assert withheld_info[0]["name"] == "Subcontracting Percentage"
+
+    # Check second item
+    assert withheld_info[1]["id"] == "sub-per-TEN-0002"
+    assert withheld_info[1]["field"] == "sub-per"
+    assert withheld_info[1]["name"] == "Subcontracting Percentage"
 
 
 if __name__ == "__main__":
